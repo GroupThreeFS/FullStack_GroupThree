d2ddac8a7f4936cb5842413a61e7d02574aebd48
tateys12 <tfollett53@gmail.com>
2023-08-09 13:57:44 -0230

    MONGO_CONNECTION

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,1 +1,15 @@
-/*Connection settings to mongo will go here*/
+const {mongoClient} = require('mongodb');
+require('dotenv').config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+client.connect((err)=>{
+    if (err){
+        console.error('Error connecting to MongoDB:', err);
+    } else{
+        console.log('Connected to MongoDB');
+    }
+})
+
+module.exports = client;

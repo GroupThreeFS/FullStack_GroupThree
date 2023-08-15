60021be1b30494c47d70593ccf9dacdfc2b0d496
Jon Ivany <117655972+JonnyNL@users.noreply.github.com>
2023-08-15 13:25:34 -0230

    Working mongo connection
    Doesnt display that you are connected. Access http://localhost:3000/api/Developers?db=both
    or
    http://localhost:3000/api/Developers?db=mongo to test

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
 
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect((err) => {
+  
   if (err) {
     console.error('Error connecting to MongoDB:', err);
   } else {
